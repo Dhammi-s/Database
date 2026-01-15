cdf738eacd991ed9139b6b1ee19ea3dc64f3964e
JASSADHAMMI <jassadhammi@gmail.com>
2026-01-15 22:51:22 +0530

    Update script.sql
    new

--- script.sql
+++ script.sql
@@ -1,7 +1,111 @@
-﻿USE [ecommerce]
+USE [master]
 GO
-/****** Object:  Table [dbo].[CartItems]    Script Date: 23-11-2025 20:49:47 ******/
-	 
+/****** Object:  Database [ecommerce]    Script Date: 15-01-2026 22:47:51 ******/
+CREATE DATABASE [ecommerce]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'ecommerce', FILENAME = N'D:\sql server\MSSQL16.MSSQLSERVER\MSSQL\DATA\ecommerce.mdf' , SIZE = 73728KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'ecommerce_log', FILENAME = N'D:\sql server\MSSQL16.MSSQLSERVER\MSSQL\DATA\ecommerce_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+GO
+ALTER DATABASE [ecommerce] SET COMPATIBILITY_LEVEL = 160
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [ecommerce].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [ecommerce] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [ecommerce] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [ecommerce] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [ecommerce] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [ecommerce] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [ecommerce] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [ecommerce] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [ecommerce] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [ecommerce] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [ecommerce] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [ecommerce] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [ecommerce] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [ecommerce] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [ecommerce] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [ecommerce] SET  ENABLE_BROKER 
+GO
+ALTER DATABASE [ecommerce] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [ecommerce] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [ecommerce] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [ecommerce] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [ecommerce] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [ecommerce] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [ecommerce] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [ecommerce] SET RECOVERY FULL 
+GO
+ALTER DATABASE [ecommerce] SET  MULTI_USER 
+GO
+ALTER DATABASE [ecommerce] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [ecommerce] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [ecommerce] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [ecommerce] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [ecommerce] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [ecommerce] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'ecommerce', N'ON'
+GO
+ALTER DATABASE [ecommerce] SET QUERY_STORE = ON
+GO
+ALTER DATABASE [ecommerce] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
+GO
+USE [ecommerce]
+GO
+/****** Object:  Table [dbo].[ApiLogs]    Script Date: 15-01-2026 22:47:52 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ApiLogs](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [uniqueidentifier] NULL,
+	[IpAddress] [varchar](50) NULL,
+	[Method] [varchar](10) NULL,
+	[Path] [varchar](255) NULL,
+	[RequestBody] [nvarchar](max) NULL,
+	[ResponseBody] [nvarchar](max) NULL,
+	[StatusCode] [int] NULL,
+	[CreatedAt] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[CartItems]    Script Date: 15-01-2026 22:47:52 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -24,7 +128,7 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[OrderAddresses]    Script Date: 23-11-2025 20:49:47 ******/
+/****** Object:  Table [dbo].[OrderAddresses]    Script Date: 15-01-2026 22:47:52 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -48,7 +152,7 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[OrderItems]    Script Date: 23-11-2025 20:49:47 ******/
+/****** Object:  Table [dbo].[OrderItems]    Script Date: 15-01-2026 22:47:52 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -71,7 +175,7 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[OrderPayments]    Script Date: 23-11-2025 20:49:47 ******/
+/****** Object:  Table [dbo].[OrderPayments]    Script Date: 15-01-2026 22:47:52 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -92,7 +196,7 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Orders]    Script Date: 23-11-2025 20:49:47 ******/
+/****** Object:  Table [dbo].[Orders]    Script Date: 15-01-2026 22:47:52 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -121,7 +225,7 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[OrderStatusHistory]    Script Date: 23-11-2025 20:49:47 ******/
+/****** Object:  Table [dbo].[OrderStatusHistory]    Script Date: 15-01-2026 22:47:52 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -140,31 +244,51 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Products]    Script Date: 23-11-2025 20:49:47 ******/
+/****** Object:  Table [dbo].[Products]    Script Date: 15-01-2026 22:47:52 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[Products] (
-    [ProductId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
-    [Name] NVARCHAR(255) NOT NULL,
-    [Description] NVARCHAR(MAX) NOT NULL,
-    [Price] DECIMAL(18,2) NOT NULL,
-    [Category] NVARCHAR(200) NOT NULL,
-    [InStock] BIT NOT NULL,
-    [ImageUrl] NVARCHAR(MAX) NULL,
-    [Usersid] UNIQUEIDENTIFIER NOT NULL,
-    [IsDeleted] BIT NOT NULL DEFAULT 0,
-    [IsActive] BIT NOT NULL DEFAULT 1
-);
-
+CREATE TABLE [dbo].[Products](
+	[ProductId] [uniqueidentifier] NOT NULL,
+	[Name] [nvarchar](255) NOT NULL,
+	[Description] [nvarchar](max) NOT NULL,
+	[Price] [decimal](18, 2) NOT NULL,
+	[Category] [nvarchar](200) NOT NULL,
+	[InStock] [bit] NOT NULL,
+	[ImageUrl] [nvarchar](max) NULL,
+	[Usersid] [uniqueidentifier] NOT NULL,
+	[IsDeleted] [bit] NULL,
+	[IsActive] [bit] NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[ProductId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Userr]    Script Date: 23-11-2025 20:49:47 ******/
+/****** Object:  Table [dbo].[seller]    Script Date: 15-01-2026 22:47:52 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[seller](
+	[SellerId] [uniqueidentifier] NOT NULL,
+	[Name] [nvarchar](150) NOT NULL,
+	[Phone] [varchar](10) NOT NULL,
+	[Address] [nvarchar](300) NOT NULL,
+	[Category] [nvarchar](100) NOT NULL,
+	[Email] [nvarchar](150) NOT NULL,
+	[Gender] [nvarchar](20) NOT NULL,
+	[PAN] [varchar](10) NOT NULL,
+	[Aadhaar] [varchar](12) NOT NULL,
+	[CreatedDate] [datetime] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[SellerId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Userr]    Script Date: 15-01-2026 22:47:52 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -186,7 +310,7 @@ PRIMARY KEY CLUSTERED
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[users]    Script Date: 23-11-2025 20:49:47 ******/
+/****** Object:  Table [dbo].[users]    Script Date: 15-01-2026 22:47:52 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -194,6 +318,8 @@ GO
 CREATE TABLE [dbo].[users](
 	[id] [int] NULL
 ) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ApiLogs] ADD  DEFAULT (getdate()) FOR [CreatedAt]
 GO
 ALTER TABLE [dbo].[CartItems] ADD  DEFAULT (newid()) FOR [CartItemID]
 GO
@@ -243,6 +369,14 @@ ALTER TABLE [dbo].[OrderStatusHistory] ADD  DEFAULT (sysdatetime()) FOR [Created
 GO
 ALTER TABLE [dbo].[Products] ADD  DEFAULT ((0)) FOR [InStock]
 GO
+ALTER TABLE [dbo].[Products] ADD  DEFAULT ((0)) FOR [IsDeleted]
+GO
+ALTER TABLE [dbo].[Products] ADD  DEFAULT ((1)) FOR [IsActive]
+GO
+ALTER TABLE [dbo].[seller] ADD  DEFAULT (newid()) FOR [SellerId]
+GO
+ALTER TABLE [dbo].[seller] ADD  DEFAULT (getdate()) FOR [CreatedDate]
+GO
 ALTER TABLE [dbo].[Userr] ADD  DEFAULT ((0)) FOR [IsAdmin]
 GO
 ALTER TABLE [dbo].[CartItems]  WITH CHECK ADD  CONSTRAINT [FK_CartItem_Product] FOREIGN KEY([ProductId])
@@ -285,5 +419,28 @@ REFERENCES [dbo].[Userr] ([Id])
 GO
 ALTER TABLE [dbo].[Products] CHECK CONSTRAINT [FK_Product_Users]
 GO
-
-
+/****** Object:  StoredProcedure [dbo].[GetAllProducts]    Script Date: 15-01-2026 22:47:52 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[GetAllProducts]
+AS
+BEGIN
+    SELECT 
+        ProductId,
+        Name,
+        Description,
+        Price,
+        Category,
+        InStock,
+        ImageUrl,
+        UsersId    -- Add this
+    FROM Products
+    WHERE Category = 'Electronics'
+END
+GO
+USE [master]
+GO
+ALTER DATABASE [ecommerce] SET  READ_WRITE 
+GO
